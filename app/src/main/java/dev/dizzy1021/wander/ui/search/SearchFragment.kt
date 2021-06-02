26e0d727b8dbabcd6184f4704a71472b9cfda5de
dizzy1021 <dimasadiprananda@gmail.com>
2021-06-02 20:10:29 +0700

    Add LocationProvider to get Position user

--- app/src/main/java/dev/dizzy1021/wander/ui/search/SearchFragment.kt
+++ app/src/main/java/dev/dizzy1021/wander/ui/search/SearchFragment.kt
@@ -1,17 +1,24 @@
 package dev.dizzy1021.wander.ui.search
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.provider.Settings
 import android.view.*
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
 import androidx.core.content.FileProvider
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
@@ -20,6 +27,8 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import dagger.hilt.android.AndroidEntryPoint
 import dev.dizzy1021.core.adapter.PlaceAdapter
 import dev.dizzy1021.core.adapter.PlaceLoadStateAdapter
@@ -60,12 +69,70 @@ class SearchFragment : Fragment() {
     private var inputStream: InputStream? = null
     private var photoFile: File? = null
 
+    private lateinit var fusedLocation: FusedLocationProviderClient
+
     @Inject
     lateinit var pref: SharedPreferenceUtil
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+
+        fusedLocation = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        getPosition()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getPosition() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                fusedLocation.lastLocation
+                    .addOnSuccessListener { loc: Location? ->
+                        lifecycleScope.launch {
+                            viewModel.queryChannel.send("geo:${loc?.latitude.toString()},${loc?.longitude.toString()}")
+                        }
+                    }
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Please turn on" + " your location...",
+                    Toast.LENGTH_LONG
+                )
+                    .show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(), arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ), 55
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -134,10 +201,6 @@ class SearchFragment : Fragment() {
                 navigateToDetailPlace(data.id)
             }
         })
-
-        lifecycleScope.launch {
-            viewModel.queryChannel.send("test")
-        }
 
         observeData()
 
